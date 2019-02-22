9576c762062b2c599ce47667d172e7de94fba2e9
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2019-02-22 12:50:25 +0100

    removed inlite storybook demo

--- stories/index.ts
+++ stories/index.ts
@@ -5,27 +5,11 @@ import { Editor } from '../src/components/Editor';
 import { content } from './fakeContent';
 
 storiesOf('TestComponent', module)
-  .add('inline in init', () => ({
-    components: { Editor },
-    data: () => ({ content }),
-    template: `<div>
-    <editor :init="{inline: true}" v-model="content"></editor>
-  </div>`
-  }))
   .add('inline', () => ({
     components: { Editor },
     data: () => ({ content }),
     template: `<div>
       <editor inline v-model="content" />
-      <textarea style="width: 100%;height:150px;" v-model="content"></textarea>
-      <div v-html="content"></div>
-    </div>`
-  }))
-  .add('inlite', () => ({
-    components: { Editor },
-    data: () => ({ content }),
-    template: `<div>
-      <editor inline :init="{theme: 'inlite'}" v-model="content" />
       <textarea style="width: 100%;height:150px;" v-model="content"></textarea>
       <div v-html="content"></div>
     </div>`
