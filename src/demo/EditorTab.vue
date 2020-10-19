0095d01114f144b63f97e3110600b100d44e25f3
Seb <jscasca@gmail.com>
2020-10-19 14:59:39 +1000

    INT-2226: Activate and deactivate hooks for keep-alive components

---
+++ src/demo/EditorTab.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <editor v-model="html" api-key="qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc"></editor>
+  </div>
+</template>
+
+<script>
+import Editor from "/@/main/ts/index";
+
+export default {
+  name: "EditorTab",
+  components: {
+    editor: Editor
+  },
+  data() {
+    return {
+      html: ""
+    };
+  },
+  activated() {
+    console.log(this.html);
+  }
+};
+</script>
