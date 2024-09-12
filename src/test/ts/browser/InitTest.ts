6d1df84dec54b1fd1de77d4bd33a75bcad5e23c6
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-09-13 08:51:00 +1000

    add loop to iterate each editor version for tests.

--- src/test/ts/browser/InitTest.ts
+++ src/test/ts/browser/InitTest.ts
@@ -1,8 +1,10 @@
 import { Assertions, Keyboard, Keys, Waiter } from '@ephox/agar';
 import { pRender } from '../alien/Loader';
+import { VersionLoader } from '@tinymce/miniature'; // Add this line
 import { SugarElement } from '@ephox/sugar';
-import { describe, it, afterEach } from '@ephox/bedrock-client';
+import { describe, it, afterEach, before, context } from '@ephox/bedrock-client';
 import { cleanupTinymce, VALID_API_KEY } from '../alien/TestHelper';
+import { Arr } from '@ephox/katamari';
 
 describe('Editor Component Initialization Tests', () => {
   // eslint-disable-next-line @typescript-eslint/require-await
@@ -10,59 +12,70 @@ describe('Editor Component Initialization Tests', () => {
     vmContext.editor.getBody().innerHTML = '<p>' + str + '</p>';
     Keyboard.keystroke(Keys.space(), {}, SugarElement.fromDom(vmContext.editor.getBody()) as SugarElement<Node>);
   };
+
   afterEach(() => {
     cleanupTinymce();
   });
-  it('should not be inline by default', async () => {
-    const vmContext = await pRender({}, `
-      <editor
-        :init="init"
-        ></editor>`);
-    Assertions.assertEq('Editor should not be inline', false, vmContext.editor.inline);
-  });
 
-  it('should be inline with inline attribute in template', async () => {
-    const vmContext = await pRender({}, `
-      <editor
-        :init="init"
-        :inline="true"
-        ></editor>`);
-    Assertions.assertEq('Editor should be inline', true, vmContext.editor.inline);
-  });
+  Arr.each([ '4', '5', '6', '7' as const ], (version) => {
+    context(`Version: ${version}`, () => {
+      before(async () => {
+        await VersionLoader.pLoadVersion(version);
+      });
 
-  it('should be inline with inline option in init', async () => {
-    const vmContext = await pRender({ init: { inline: true }});
-    Assertions.assertEq('Editor should be inline', true, vmContext.editor.inline);
-  });
-  it('should handle one-way binding with output-format="text"', async () => {
-    const vmContext = await pRender({
-      content: undefined,
-    }, `
-      <editor
-        :init="init"
-        api-key="${VALID_API_KEY}"
-        @update:modelValue="content =$event"
-        output-format="text"
-      ></editor>
-    `);
-    await pFakeType('A', vmContext);
-    await Waiter.pWait(100);
-    Assertions.assertEq('Content emitted should be of format="text"', 'A', vmContext.vm.content);
-  });
+      it('should not be inline by default', async () => {
+        const vmContext = await pRender({}, `
+          <editor
+            :init="init"
+          ></editor>`);
+        Assertions.assertEq('Editor should not be inline', false, vmContext.editor.inline);
+      });
 
-  it('should handle one-way binding with output-format="html"', async () => {
-    const vmContext = await pRender({
-      content: undefined,
-    }, `
-      <editor
-        :init="init"
-        api-key="${VALID_API_KEY}"
-        @update:modelValue="content =$event"
-        output-format="html"
-      ></editor>
-    `);
-    await pFakeType('A', vmContext);
-    await Waiter.pWait(100);
-    Assertions.assertEq('Content emitted should be of format="html"', '<p>A</p>', vmContext.vm.content);
+      it('should be inline with inline attribute in template', async () => {
+        const vmContext = await pRender({}, `
+          <editor
+            :init="init"
+            :inline="true"
+          ></editor>`);
+        Assertions.assertEq('Editor should be inline', true, vmContext.editor.inline);
+      });
+
+      it('should be inline with inline option in init', async () => {
+        const vmContext = await pRender({ init: { inline: true }});
+        Assertions.assertEq('Editor should be inline', true, vmContext.editor.inline);
+      });
+
+      it('should handle one-way binding with output-format="text"', async () => {
+        const vmContext = await pRender({
+          content: undefined,
+        }, `
+          <editor
+            :init="init"
+            api-key="${VALID_API_KEY}"
+            @update:modelValue="content=$event"
+            output-format="text"
+          ></editor>
+        `);
+        await pFakeType('A', vmContext);
+        await Waiter.pWait(100);
+        Assertions.assertEq('Content emitted should be of format="text"', 'A', vmContext.vm.content);
+      });
+
+      it('should handle one-way binding with output-format="html"', async () => {
+        const vmContext = await pRender({
+          content: undefined,
+        }, `
+          <editor
+            :init="init"
+            api-key="${VALID_API_KEY}"
+            @update:modelValue="content=$event"
+            output-format="html"
+          ></editor>
+        `);
+        await pFakeType('A', vmContext);
+        await Waiter.pWait(100);
+        Assertions.assertEq('Content emitted should be of format="html"', '<p>A</p>', vmContext.vm.content);
+      });
+    });
   });
 });
